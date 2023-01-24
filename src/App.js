221f713f7650f77a2d7c1ed559ba6d495d84bcb6
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2023-01-23 23:08:33 -0500

    fixed issues with node modules, added header component and background image jpg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header"
 
 function App() {
   return (
-   <Navbar/>
+    <>
+      <Navbar />
+      <Header />
+      {/* <About />
+      <Projects />
+      <Resume />
+      <Contact /> */}
+    </>
   );
 }
 

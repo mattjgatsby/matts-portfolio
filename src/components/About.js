0a1e808e73c6eda9d8ac4af251a93063d72c0564
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2023-01-24 00:40:56 -0500

    added self image, started about section, added custom styling for about section in app.css

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,29 @@
 import React from "react";
+import mePicture from "../Me2019.png";
 
 const About = () => {
-    return (
-        <div></div>
-    );
-}
+  return (
+    <div className="background" href="#about">
+      <h1 className="text-center header_font_size">Matthew Gatsby</h1>
+      <div className="row d-flex justify-content-center">
+        <div className="col-sm-4">
+          <img
+            className="custom_about_image "
+            src={mePicture}
+            alt="me from 2019"
+          ></img>
+        </div>
+        <div className="col-sm-5">
+          <p className="text-right m-4 custom_about_text">
+            Passion for code in my humble abode! 28 year old, living in Northern
+            Virginia. If I'm not at my computer I'm spending time with my three
+            dogs or rounding up the frogs. I'm dedicated to learning and growing
+            my abilities as a web developer!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#about">
                 About Me
               </a>
             </li>

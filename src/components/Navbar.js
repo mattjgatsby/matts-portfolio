2d46e4a329ea35cdba5402170c4d8f23469055fc
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2023-01-23 21:56:05 -0500

    editting nav bar style, added custom logo png to src folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,35 @@
 import React from "react";
+import myLogo from "../myLogo.png"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#"><img src={myLogo} alt="my custom logo" />
         </a>
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-          </ul>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ml-auto">
+            <a className="nav-link active" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-link" href="#">
+              Features
+            </a>
+            <a className="nav-link" href="#">
+              Pricing
+            </a>
+          </div>
         </div>
       </div>
     </nav>
